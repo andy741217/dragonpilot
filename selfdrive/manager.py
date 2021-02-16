e253cf3a535371b214319faa20a43bf89dfb761b
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-02-16 21:51:09 +0800

    Update manager.py

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -439,7 +439,7 @@ def manager_thread():
   cloudlog.info({"environ": os.environ})
 
   # save boot log
-  subprocess.call(["./loggerd", "--bootlog"], cwd=os.path.join(BASEDIR, "selfdrive/loggerd"))
+  #subprocess.call(["./loggerd", "--bootlog"], cwd=os.path.join(BASEDIR, "selfdrive/loggerd"))
 
   # start daemon processes
   for p in daemon_processes:
@@ -561,7 +561,7 @@ def main():
 
   if os.getenv("PREPAREONLY") is not None:
     return
-
+  del managed_processes['loggerd']
   # SystemExit on sigterm
   signal.signal(signal.SIGTERM, lambda signum, frame: sys.exit(1))
 
