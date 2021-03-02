5b087c8748701ce44549e4a5fae769203deb1b90
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-03-02 15:55:47 +0800

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -337,6 +337,6 @@ class CarInterface(CarInterfaceBase):
                                c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
                                c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart,
                                c.hudControl.setSpeed, c.hudControl.leadVisible, c.hudControl.leadDistance,
-                               c.hudControl.leadvRel, c.hudControl.leadyRel)
+                               c.hudControl.leadvRel, c.hudControl.leadyRel, , self.dragonconf)
     self.frame += 1
     return can_sends
