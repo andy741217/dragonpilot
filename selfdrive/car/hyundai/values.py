d56cbab8dd734a7e8941f9696ac8776e4fc9d2a1
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-04-07 09:16:50 +0800

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -7,9 +7,9 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 class CarControllerParams:
   def __init__(self, CP):
-    self.STEER_MAX = 4
-    self.STEER_DELTA_UP = 3
-    self.STEER_DELTA_DOWN = 7
+    self.STEER_MAX = 417
+    self.STEER_DELTA_UP = 4
+    self.STEER_DELTA_DOWN = 8
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
     self.STEER_DRIVER_FACTOR = 1
