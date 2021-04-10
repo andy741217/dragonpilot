ba93cdf7ce08f252ae2fe13824eb5d7836e395de
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-04-10 08:29:49 +0800

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -8,7 +8,7 @@ Ecu = car.CarParams.Ecu
 class CarControllerParams:
   def __init__(self, CP):
     self.STEER_MAX = 417
-    self.STEER_DELTA_UP = 6
+    self.STEER_DELTA_UP = 5
     self.STEER_DELTA_DOWN = 5
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
