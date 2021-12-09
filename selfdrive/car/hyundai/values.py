4dbf20e94562d65dd57bea91bc86810a60c0ce10
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-12-09 21:50:43 +0800

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -12,7 +12,7 @@ class CarControllerParams:
 
   def __init__(self, CP):
     if Params().get('dp_hkg_smart_mdps') == b'1':
-      self.STEER_MAX = 384
+      self.STEER_MAX = 409
     elif CP.carFingerprint in [CAR.SONATA, CAR.PALISADE, CAR.SANTA_FE, CAR.VELOSTER, CAR.GENESIS_G70,
                              CAR.IONIQ_EV_2020, CAR.KIA_CEED, CAR.KIA_SELTOS, CAR.ELANTRA_2021,
                              CAR.ELANTRA_HEV_2021, CAR.SONATA_HYBRID, CAR.KONA_HEV, CAR.SANTA_FE_2022, CAR.KIA_K5_2021, CAR.KONA_EV, CAR.KONA, CAR.IONIQ_HEV_2022]:
