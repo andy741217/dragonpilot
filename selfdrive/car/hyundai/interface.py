08bb2cacbcba9ad0131d55f74b362eec072e4d2c
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-12-11 12:09:51 +0800

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -35,10 +35,10 @@ class CarInterface(CarInterfaceBase):
     # Most Hyundai car ports are community features for now
     ret.communityFeature = candidate not in [CAR.SONATA, CAR.PALISADE]
 
-    ret.steerActuatorDelay = 0.1  # Default delay not used, check pathplanner for BPs
+    ret.steerActuatorDelay = 0.3  # Default delay not used, check pathplanner for BPs
     ret.steerRateCost = 0.4
     ret.steerLimitTimer = 1.5
-    ret.steerRatio = 16.5
+    ret.steerRatio = 15.5
     tire_stiffness_factor = 1.
 
     
@@ -113,7 +113,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1685. + STD_CARGO_KG
       ret.wheelbase = 2.7
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1700.
+      ret.lateralTuning.lqr.scale = 2500.
       ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
