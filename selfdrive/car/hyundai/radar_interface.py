60fe372e46cde1537fcc9a35271964b204eaee1b
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-12-13 21:35:26 +0800

    Add files via upload

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
+import math
+
 from cereal import car
 from opendbc.can.parser import CANParser
 from selfdrive.car.interfaces import RadarInterfaceBase
 from selfdrive.car.hyundai.values import DBC
 
+RADAR_START_ADDR = 0x500
+RADAR_MSG_COUNT = 32
+
 
 def get_radar_can_parser(CP):
-  signals = [
-    # sig_name, sig_address, default
-    ("ObjValid", "SCC11", 0),
+  if DBC[CP.carFingerprint]['radar'] is None:
+    return None
+
+  signals = []
+  checks = []
+
+  for addr in range(RADAR_START_ADDR, RADAR_START_ADDR + RADAR_MSG_COUNT):
+    msg = f"RADAR_TRACK_{addr:x}"
+    signals += [
+      ("ObjValid", "SCC11", 0),
     ("ACC_ObjLatPos", "SCC11", 0),
     ("ACC_ObjDist", "SCC11", 0),
     ("ACC_ObjRelSpd", "SCC11", 0),
-  ]
-  checks = [
-    # address, frequency
-    ("SCC11", 50),
-  ]
-  return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 2)  #only needed when scc radar on bus 2
+    ]
+    checks += [("SCC11", 50)]
+  return CANParser(DBC[CP.carFingerprint]['radar'], signals, checks, 2)
 
 
 class RadarInterface(RadarInterfaceBase):
   def __init__(self, CP):
     super().__init__(CP)
-    self.rcp = get_radar_can_parser(CP)
     self.updated_messages = set()
-    self.trigger_msg = 0x420
+    self.trigger_msg = RADAR_START_ADDR + RADAR_MSG_COUNT - 1
     self.track_id = 0
-    self.radar_off_can = CP.sccBus != 2
+
+    self.radar_off_can = CP.radarOffCan
+    self.rcp = get_radar_can_parser(CP)
 
   def update(self, can_strings):
-    if self.radar_off_can:
+    if self.radar_off_can or (self.rcp is None):
       return super().update(None)
 
     vls = self.rcp.update_strings(can_strings)
     self.updated_messages.update(vls)
 
     if self.trigger_msg not in self.updated_messages:
-      return car.RadarData.new_message()
+      return None
 
     rr = self._update(self.updated_messages)
     self.updated_messages.clear()
@@ -46,14 +56,25 @@ class RadarInterface(RadarInterfaceBase):
 
   def _update(self, updated_messages):
     ret = car.RadarData.new_message()
-    cpt = self.rcp.vl
+    if self.rcp is None:
+      return ret
+
     errors = []
+
     if not self.rcp.can_valid:
       errors.append("canError")
     ret.errors = errors
 
-    valid = cpt["SCC11"]['ObjValid']
-    if valid:
+    for addr in range(RADAR_START_ADDR, RADAR_START_ADDR + RADAR_MSG_COUNT):
+      msg = self.rcp.vl[f"RADAR_TRACK_{addr:x}"]
+
+      if addr not in self.pts:
+        self.pts[addr] = car.RadarData.RadarPoint.new_message()
+        self.pts[addr].trackId = self.track_id
+        self.track_id += 1
+
+      valid = cpt["SCC11"]['ObjValid']
+      if valid:
       for ii in range(1):
         if ii not in self.pts:
           self.pts[ii] = car.RadarData.RadarPoint.new_message()
