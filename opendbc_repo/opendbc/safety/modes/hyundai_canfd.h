9cde17a970ded26927c72ded01900c5741037d75
andy741217 <76022969+andy741217@users.noreply.github.com>
2026-09-03 19:38:30 +0800

    Update hyundai_canfd.h

--- opendbc_repo/opendbc/safety/modes/hyundai_canfd.h
+++ opendbc_repo/opendbc/safety/modes/hyundai_canfd.h
@@ -137,11 +137,11 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *msg) {
 
 static bool hyundai_canfd_tx_hook(const CANPacket_t *msg) {
   const TorqueSteeringLimits HYUNDAI_CANFD_STEERING_LIMITS = {
-    .max_torque = 270,
+    .max_torque = 330,
     .max_rt_delta = 112,
-    .max_rate_up = 2,
-    .max_rate_down = 3,
-    .driver_torque_allowance = 250,
+    .max_rate_up = 10,
+    .max_rate_down = 10,
+    .driver_torque_allowance = 350,
     .driver_torque_multiplier = 2,
     .type = TorqueDriverLimited,
 
